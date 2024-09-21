7668a17574634d1654256977a740f8ca8ad500aa
Vander Gerald <vander.gerald@ui.ac.id>
2024-09-21 19:40:52 +0700

    try fix

--- main/views.py
+++ main/views.py
@@ -57,6 +57,7 @@ def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+
 def register(request):
     form = UserCreationForm()
     if request.method == "POST":
